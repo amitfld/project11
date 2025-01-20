740e910e6614f99d8dc259d2653e814911354ddc
Amit Feldman <amit.feldman@post.runi.ac.il>
2025-01-20 16:25:15 +0200

    Changes to VMWriter

--- VMWriter.py
+++ VMWriter.py
@@ -1,109 +1,50 @@
-"""
-This file is part of nand2tetris, as taught in The Hebrew University, and
-was written by Aviv Yaish. It is an extension to the specifications given
-[here](https://www.nand2tetris.org) (Shimon Schocken and Noam Nisan, 2017),
-as allowed by the Creative Common Attribution-NonCommercial-ShareAlike 3.0
-Unported [License](https://creativecommons.org/licenses/by-nc-sa/3.0/).
-"""
 import typing
 
 
 class VMWriter:
     """
-    Writes VM commands into a file. Encapsulates the VM command syntax.
+    Writes VM commands into a file.
     """
 
     def __init__(self, output_stream: typing.TextIO) -> None:
         """Creates a new file and prepares it for writing VM commands."""
-        # Your code goes here!
-        # Note that you can write to output_stream like so:
-        # output_stream.write("Hello world! \n")
         self.output_stream = output_stream
 
     def write_to_file(self, command):
         self.output_stream.write(command + '\n')
 
     def write_push(self, segment: str, index: int) -> None:
-        """Writes a VM push command.
-
-        Args:
-            segment (str): the segment to push to, can be "CONST", "ARG", 
-            "LOCAL", "STATIC", "THIS", "THAT", "POINTER", "TEMP"
-            index (int): the index to push to.
-        """
-        # Your code goes here!
+        """Writes a VM push command."""
         self.write_to_file(f"push {segment} {index}")
 
-
     def write_pop(self, segment: str, index: int) -> None:
-        """Writes a VM pop command.
-
-        Args:
-            segment (str): the segment to pop from, can be "CONST", "ARG", 
-            "LOCAL", "STATIC", "THIS", "THAT", "POINTER", "TEMP".
-            index (int): the index to pop from.
-        """
-        # Your code goes here!
+        """Writes a VM pop command."""
         self.write_to_file(f"pop {segment} {index}")
 
     def write_arithmetic(self, command: str) -> None:
-        """Writes a VM arithmetic command.
-
-        Args:
-            command (str): the command to write, can be "ADD", "SUB", "NEG", 
-            "EQ", "GT", "LT", "AND", "OR", "NOT", "SHIFTLEFT", "SHIFTRIGHT".
-        """
-        # Your code goes here!
+        """Writes a VM arithmetic command."""
         self.write_to_file(command)
 
     def write_label(self, label: str) -> None:
-        """Writes a VM label command.
-
-        Args:
-            label (str): the label to write.
-        """
-        # Your code goes here!
+        """Writes a VM label command."""
         self.write_to_file(f"label {label}")
 
     def write_goto(self, label: str) -> None:
-        """Writes a VM goto command.
-
-        Args:
-            label (str): the label to go to.
-        """
-        # Your code goes here!
+        """Writes a VM goto command."""
         self.write_to_file(f"goto {label}")
 
     def write_if(self, label: str) -> None:
-        """Writes a VM if-goto command.
-
-        Args:
-            label (str): the label to go to.
-        """
-        # Your code goes here!
+        """Writes a VM if-goto command."""
         self.write_to_file(f"if-goto {label}")
 
     def write_call(self, name: str, n_args: int) -> None:
-        """Writes a VM call command.
-
-        Args:
-            name (str): the name of the function to call.
-            n_args (int): the number of arguments the function receives.
-        """
-        # Your code goes here!
+        """Writes a VM call command."""
         self.write_to_file(f"call {name} {n_args}")
 
     def write_function(self, name: str, n_locals: int) -> None:
-        """Writes a VM function command.
-
-        Args:
-            name (str): the name of the function.
-            n_locals (int): the number of local variables the function uses.
-        """
-        # Your code goes here!
+        """Writes a VM function command."""
         self.write_to_file(f"function {name} {n_locals}")
 
     def write_return(self) -> None:
         """Writes a VM return command."""
-        # Your code goes here!
         self.write_to_file("return")
